180497a64f456df8274741609817841200caf71e
Benjamin Smerd <benjaminsmerd@Benjamins-MBP.lan>
2024-12-05 19:31:49 +1030

    setup app with protected layouts and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,10 @@
-import { ReactNode, useState } from 'react';
+import { FC, ReactNode, useState } from 'react';
 import Login from './pages/Login';
+import useUserContext from './hooks/useUserContext';
+import SettingsProvider from './contexts/SettingsContext';
+import Navbar from './components/common/Nav/Navbar';
+import Header from './components/common/Header/Header';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 interface ProtectedRouteInterface {
   	children: ReactNode;
@@ -9,15 +14,36 @@ interface ProtextedLayoutInterface {
 	children: ReactNode;
 }
 
+const ProtecectedRoute: FC<ProtectedRouteInterface> = ({children}) => {
+	const {isLoggedIn} = useUserContext();
+
+	if(!isLoggedIn) {
+		return <Navigate to="/login" />;
+	}
+	return <ProtectedLayout>{children}</ProtectedLayout>;
+}
+
+const ProtectedLayout: FC<ProtextedLayoutInterface> = ({children}) => {
+	return (
+		<SettingsProvider>
+			<div className='flex flex-col justify-between w-full h-full'>
+				<Header />
+				<main className='h-full w-full p-1 flex-1 overflow0auto'>
+					{children}
+				</main>
+				<Navbar />
+			</div>
+		</SettingsProvider>
+	)
+}
+
 function App() {
 
-    const testFunc = () => {
-        console.log("here");
-    }
-
 	return (
-		<div>
-			<p>Hey</p>
+		<div className='h-screen w-screen font-poppins bg-slate-900'>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+			</Routes>
 		</div>
 	)
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import UserProvider from './contexts/UserContext.tsx'
-import SettingsProvider from './contexts/SettingsContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <UserProvider>
-    <SettingsProvider>    
       <StrictMode>
         <App />
       </StrictMode>
-    </SettingsProvider>
   </UserProvider>
 )
