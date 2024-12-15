7872aa216f90fbb8b86e62f955e0e4d1fca80c7f
Benjamin Smerd <benjaminsmerd@Mac.lan>
2024-12-15 12:26:13 +1030

    add in the login logic in authController

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -10,3 +10,16 @@ const signup = async (req, res) => {
         res.status(err.response?.status || 500).json({message: err.message});
     }
 }
+
+const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const response = await loginUser({email, password});
+        res.status(200).json(response.data);
+    } catch(err) {
+        res,status(err.response?.status || 500).json({message: err.message});
+    }
+}
+
+module.exports = {signup, login};
