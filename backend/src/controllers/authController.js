549f69c7978c2bf55490d7d7325922c5ddfb076f
Benjamin Smerd <benjaminsmerd@Mac.lan>
2024-12-18 21:28:15 +1030

    change authController to OOP

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,25 +1,28 @@
 const {registerUser, loginUser} = require('../services/authService');
 
-const signup = async (req, res) => {
-    const {email, password} = req.body;
-
-    try {
-        const response = await registerUser({email, password});
-        res.status(201).json(response.data);
-    } catch(err) {
-        res.status(err.response?.status || 500).json({message: err.message});
+class AuthController {
+    
+    async signup (req, res) {
+        const {email, password} = req.body;
+    
+        try {
+            const response = await registerUser({email, password});
+            res.status(201).json(response.data);
+        } catch(err) {
+            res.status(err.response?.status || 500).json({message: err.message});
+        }
+    }
+    
+    async login (req, res) {
+        const {email, password} = req.body;
+    
+        try {
+            const response = await loginUser({email, password});
+            res.status(200).json(response.data);
+        } catch(err) {
+            res,status(err.response?.status || 500).json({message: err.message});
+        }
     }
 }
 
-const login = async (req, res) => {
-    const {email, password} = req.body;
-
-    try {
-        const response = await loginUser({email, password});
-        res.status(200).json(response.data);
-    } catch(err) {
-        res,status(err.response?.status || 500).json({message: err.message});
-    }
-}
-
-module.exports = {signup, login};
+module.exports = new AuthController();
