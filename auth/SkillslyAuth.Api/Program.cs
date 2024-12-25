cfe9583e314d6e3b42dd0017fc7d554bd29950b0
Benjamin Smerd <benjaminsmerd@Mac.lan>
2024-12-25 16:55:28 +1030

    setup the connection with the EF Core framework

--- auth/SkillslyAuth.Api/Program.cs
+++ auth/SkillslyAuth.Api/Program.cs
@@ -1,17 +1,26 @@
+using AuthService.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionSting("DefaultConnection");
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
 
 // app.UseHttpsRedirection();
-
+app.MapControllers();
 app.Run();
