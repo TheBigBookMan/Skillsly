ee13fa7f21a611120993a7f865a034395ed22ac1
Benjamin Smerd <benjaminsmerd@Mac.lan>
2024-12-25 21:14:38 +1030

    setup usetdetailsdto

---
+++ auth/SkillslyAuth.Application/DTOs/UserDetailsDto.cs
@@ -0,0 +1,9 @@
+namespace AuthService.Application.DTOs;
+
+public class UserDetailsDto
+{
+    public Guid Id {get; set;}
+    public string Email {get; set;}
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
+}
