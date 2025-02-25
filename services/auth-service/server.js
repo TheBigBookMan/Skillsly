8216ba43592a28d3cd8c1be90383c9136589b823
Ben Smerd <bsmerd@adam.com.au>
2025-02-25 20:50:19 +1030

    init commit and server.js in auth setup

---
+++ services/auth-service/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+// * Middleware
+app.use(cors());
+app.use(express.json());
+
+// ! Test
+app.get("/", (req, res) => {
+    res.send({ message: "Auth service running" });
+});
+
+// * Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Auth server running on port ${PORT}`));
