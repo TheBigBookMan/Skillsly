a49057b471fde1837403af2047a6c75283bccfd8
Benjamin Smerd <benjaminsmerd@Mac.lan>
2025-03-01 21:05:19 +1030

    setup nodemon in docker

--- services/auth-service/server.js
+++ services/auth-service/server.js
@@ -17,7 +17,8 @@ app.get("/", (req, res) => {
 });
 
 const verifyToken = async (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1];
+    console.log(req.headers);
+    const token = req.headers.authorization;
 
     if(!token) {
         return res.status(401).json({ error: "Unauthorised" });
