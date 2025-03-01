f0085d0be3f3df08ff62b48129af1d794f6550b0
Benjamin Smerd <benjaminsmerd@Mac.lan>
2025-03-01 21:47:15 +1030

    setup JWT auth in auth middleware

--- services/auth-service/server.js
+++ services/auth-service/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const admin = require("./firebase-config");
+const jwt = require("jsonwebtoken");
 
 dotenv.config();
 
@@ -11,32 +12,64 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ! Test
-app.get("/", (req, res) => {
-    res.send({ message: "Auth service running" });
-});
+// * Verify firebase token from auth head
+const verifyFirebaseToken = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
 
-const verifyToken = async (req, res, next) => {
-    const token = req.headers.authorization;
-
-    if(!token) {
-        return res.status(401).json({ error: "Unauthorised" });
+    if(!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ error: "Unauthorised: No token provided" });
     }
+
+    const firebaseToken = authHeader.split(" ")[1];
 
     try {
 
-        const decodedToken = await admin.auth().verifyIdToken(token);
+        const decodedToken = await admin.auth().verifyIdToken(firebaseToken);
         req.user = decodedToken;
         next();
 
     } catch(err) {
-        res.status(401).json({ error: "Invalid token", message: err });
+        res.status(401).json({ error: "Unauthorised: Invalid Firebase Token" });
     }
 }
 
+// * Verify JWT token from auth header
+const verifyJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ error: "Unathorised: No JWT Token" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+
+    } catch(err) {
+        res.status(401).json({ error: "Unauthorised: Invalid JWT" });
+    }
+}
+
+// * Login
+app.post("/auth/login", verifyFirebaseToken, (req, res) => {
+    const user = req.user;
+
+    const jwtToken = jwt.sign(
+        { uid: user.uid, email: user.email, role: user.role || "user" },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.json({ jwtToken });
+});
+
 // * Protected route
-app.get("/protected", verifyToken, (req, res) => {
-    res.json({ message: "You are authenticated.", user: req.user });
+app.get("/protected", verifyJwt, (req, res) => {
+    res.json({ message: "You are authenticated with JWT.", user: req.user });
 })
 
 // * Start server
