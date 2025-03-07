e091a9e832b8188cda0f1ef29acc607b8744fbbe
Benjamin Smerd <bjsmerd@gmail.com>
2025-03-07 21:32:59 +1030

    create user migration

--- services/auth-service/migrations/20250307095405_create_users_table.js
+++ services/auth-service/migrations/20250307095405_create_users_table.js
@@ -4,7 +4,7 @@
  */
 exports.up = function(knex) {
     return knex.schema.createTable("users", function(table) {
-        table.string("id"),primary();
+        table.string("id").primary();
         table.string("email").unique().notNullable();
         table.enum("role", ["user", "instructor"]).defaultTo("user");
         table.timestamp("created_at").defaultTo(knex.fn.now());
