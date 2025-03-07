0e74b3e74dfa3872823f9b6b2dcc11b194929a68
Benjamin Smerd <bjsmerd@gmail.com>
2025-03-07 21:48:50 +1030

    feat(db): add migration for onboarding_check on users table

---
+++ services/auth-service/migrations/20250307111549_add_onboarding_status_to_users.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable("users", function(table) {
+        table.boolean("onboarding_complete").defaultTo(false);
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.alterTable("users", function(table) {
+        table.dropColumn("onboarding_complete");
+    })
+};
