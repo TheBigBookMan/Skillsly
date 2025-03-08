fd792cf94b6cd389f7a71ed5919a79ac180a5846
Ben Smerd <bsmerd@adam.com.au>
2025-03-08 13:10:21 +1030

    feat(routes): add in the routes for REST requests for Users

--- services/backend/routes/userRoutes.ts
+++ services/backend/routes/userRoutes.ts
@@ -2,6 +2,14 @@ import express from 'express';
 import UserController from '../controllers/UserController';
 
 const router = express.Router();
+
+// ? /
+router.get('/', UserController.getAllUsers);
+router.post('/', UserController.createUser);
+
+// ? /:id
 router.get('/:id', UserController.getUser);
+router.put('/:id', UserController.updateUser);
+router.delete('/:id', UserController.deleteUser);
 
 export default router;
