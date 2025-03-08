6e2cd4e0eb90e3d8adccfa4f1ea7bc6d57a8e401
Ben Smerd <bsmerd@adam.com.au>
2025-03-08 13:20:20 +1030

    feat(routes): add skillsRoutes to the main router

--- services/backend/routes/routes.ts
+++ services/backend/routes/routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import userRoutes from './userRoutes';
+import skillsRoutes from './skillsRoutes';
 
 const router = express.Router();
 
 router.use("/user", userRoutes);
+router.use("/skills", skillsRoutes);
 
 export default router;
