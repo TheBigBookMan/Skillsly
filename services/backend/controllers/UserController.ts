de6e178822ec31eeaf023deafb8ba909fc61fce7
Ben Smerd <bsmerd@adam.com.au>
2025-03-08 14:12:54 +1030

    feat(controller): add in the completeDeleteUser controller in UserController

--- services/backend/controllers/UserController.ts
+++ services/backend/controllers/UserController.ts
@@ -95,6 +95,34 @@ class UserController {
             res.status(500).json({ message: "Server error" });
         }
     }
+
+    // ? Completely delete user from the database- not able to reinstate
+    async completeDeleteUser(req: Request, res: Response) {
+        const {id} = req.params;
+        
+        if(!id) return res.status(400).json({ message: "Id not given" });
+
+        try {
+
+            const [existingUser] = await pool.query("SELECT id FROM users WHERE id = ?", [id]);
+
+            if(!Array.isArray(existingUser) || existingUser.length === 0) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            const [result] = await pool.query("DELETE FROM users WHERE id = ?", [id]);
+
+            if((result as any).affectedRows === 0) {
+                return res.status(400).json({ message: "Failed to completely delete user" });
+            }
+
+            res.status(200).json({ message: "Successfully completely deleted user" });
+
+        } catch(err) {
+            console.error(err);
+            res.status(500).json({ message: 'Server error' });
+        }
+    }
 }
 
 export default new UserController();
