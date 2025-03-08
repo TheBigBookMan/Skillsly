e8615ecee89c0bf7f752ca1845aacf5edf579189
Ben Smerd <bsmerd@adam.com.au>
2025-03-08 13:41:08 +1030

    feat(routes): in userRoutes add in a reinstate and remove path REST

--- services/backend/routes/userRoutes.ts
+++ services/backend/routes/userRoutes.ts
@@ -10,6 +10,12 @@ router.post('/', UserController.createUser);
 // ? /:id
 router.get('/:id', UserController.getUser);
 router.put('/:id', UserController.updateUser);
+// * Adds a status D to the user in user table- can reinstate
 router.delete('/:id', UserController.deleteUser);
+
+// ? /userStatus/:id
+router.put('/userStatus/:id', UserController.reinstateUser);
+// * Completely remove from database
+router.delete('/userStatus/:id', UserController.completeDeleteUser);
 
 export default router;
