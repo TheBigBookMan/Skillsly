3299395844977e99e934bffafaf581e1f0428cff
Ben Smerd <bsmerd@adam.com.au>
2025-03-08 13:53:40 +1030

    feat(controller): created the deleteUser controller in UserController

--- services/backend/controllers/UserController.ts
+++ services/backend/controllers/UserController.ts
@@ -37,6 +37,33 @@ class UserController {
             res.status(500).json({ error: 'Server error' });
         }
     } 
+
+    async deleteUser(req: Request, res: Response) {
+        const {id} = req.params;
+
+        if(!id) return res.status(400).json({ error: 'Id not found' });
+
+        try {
+
+            const [existingUser] = await pool.query("SELECT id FROM users WHERE id = ?", [id]);
+
+            if(!Array.isArray(existingUser) || existingUser.length === 0) {
+                return res.status(404).json({ message: "User not found" });
+            }
+
+            const [result] = await pool.query("UPDATE users SET Status = 'D' WHERE id = ?", [id]);
+
+            if((result as any).affectedRows === 0) {
+                return res.status(400).json({ error: 'Failed to delete user' });
+            }
+
+            res.status(200).json({ message: "Successfully deleted user" });
+
+        } catch(err) {
+            console.error(err);
+            res.status(500).json({ message: 'Server error' });
+        }
+    }
 }
 
 export default new UserController();
