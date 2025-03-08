57d81403488bef882686feece0e843ebe5fd4802
Ben Smerd <bsmerd@adam.com.au>
2025-03-08 13:31:48 +1030

    feat(controller): create the getAllSkills controller in SkillController

--- services/backend/controllers/SkillController.ts
+++ services/backend/controllers/SkillController.ts
@@ -5,15 +5,32 @@ class SkillController {
     async getSkill(req: Request, res: Response) {
         try {
 
-            const [rows] = await pool.query("SELECT * FROM skills WHERE id = ?", [
+            const [skill] = await pool.query("SELECT * FROM skills WHERE id = ?", [
                 req.params.id
             ]);
 
-            if(!rows || (Array.isArray(rows) && rows.length === 0)) {
+            if(!skill || (Array.isArray(skill) && skill.length === 0)) {
                 return res.status(404).json({ error: 'Skill not found' });
             }
 
-            res.json(rows);
+            res.json(skill);
+
+        } catch(err) {
+            console.error(err);
+            res.status(500).json({ error: 'Server error' });
+        }
+    }
+
+    async getAllSkills(req: Request, res: Response) {
+        try {
+
+            const [skills] = await pool.query("SELECT * FROM skills");
+
+            if(!skills || (Array.isArray(skills) && skills.length === 0)) {
+                return res.status(404).json({ error: 'No skills found' });
+            }
+
+            res.json(skills)
 
         } catch(err) {
             console.error(err);
