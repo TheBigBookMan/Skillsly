dad11c02e14d0327df603cd2d8cce0f272e5dabe
Ben Smerd <bsmerd@adam.com.au>
2025-03-20 21:00:16 +1030

    feature(frontend): make the header have some nav items

--- services/frontend/src/components/common/Header.tsx
+++ services/frontend/src/components/common/Header.tsx
@@ -1,8 +1,38 @@
+import Link from "next/link";
+import { useAuth } from "@/context/AuthContext";
+
+// TODO need to make mobile responsive with hamburger
+
+
 const Header = () => {
+    const {logout} = useAuth();
+
+    const onLogout = () => {
+        const confirmation = confirm("Are you sure you want to logout?");
+
+        return !confirmation && logout();
+    }
+
     return (
-        <div>
-            <p>Header</p>
-        </div>
+        <header className="w-full flex justify-around">
+            <div className='flex'>
+                <Link href="/">
+                    {/* <img /> */}
+                </Link>
+            </div>
+
+            <div className='flex gap-6'>
+                <Link href="/skills" >Skills</Link>
+                <Link href="/profile/[profileId]" >Profile</Link>
+                <Link href="/inbox">Inbox</Link>
+                <Link href="/lessons">Lessons</Link>
+            </div>
+
+            <div className='flex gap-10'>
+                <Link href="/settings">Settings</Link>
+                <p onClick={onLogout}>Logout</p>
+            </div>
+        </header>
     )
 }
 
