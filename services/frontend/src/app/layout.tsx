ce091f8143c460e722aee8f1cf06d1431ed8b70c
Ben Smerd <bsmerd@adam.com.au>
2025-03-20 19:45:32 +1030

    feater(layout): create a header and footer and add them to the layout, which conditionally will render based on authed based pages

--- services/frontend/src/app/layout.tsx
+++ services/frontend/src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { AuthProvider } from "@/context/AuthContext";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
+import { usePathname } from "next/navigation";
 
 export const metadata: Metadata = {
     title: 'Skillsly',
@@ -15,12 +18,23 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+    const pathname = usePathname();
+
+    const authPages = ["/login", "/register"];
+
     return (
         <html lang="en">
             <body>
                 <QueryClientProvider client={queryClient}>
                     <AuthProvider>
-                        {children}
+                        <main className="min-h-screen flex flex-col">
+                            {!authPages.includes(pathname) && <Header />}
+
+                            {children}
+
+                            {!authPages.includes(pathname) && <Footer />}
+                        </main>
                     </AuthProvider>
                 </QueryClientProvider>
             </body>
---
+++ services/frontend/src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            <p>Foorter</p>
+        </div>
+    )
+}
+
+export default Footer;
